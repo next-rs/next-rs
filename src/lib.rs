0df3aa69cc1d561862cf1fddf801ccb77857ed71
wiseaidev <oss@wiseai.dev>
2024-01-27 20:01:18 +0200

    re-export `yew_i18n::YewI18n`

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,6 @@ pub use yew::prelude;
 pub use yew::Renderer;
 pub use yew_router::prelude as router;
 pub use yew_navbar::{Menu, Navbar, NavbarProps};
-pub use yew_i18n::{I18nProvider, use_translation};
+pub use yew_i18n::{I18nProvider, YewI18n, use_translation};
 pub use yew_sidebar::{MenuItem, Sidebar, SidebarProps};
 pub use input_yew::CustomInput as Input;
