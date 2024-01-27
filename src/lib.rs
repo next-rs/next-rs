4d33f2c1b1411f0ab57fd78c375cb5eb9f010d9a
wiseaidev <oss@wiseai.dev>
2024-01-27 19:14:49 +0200

    add and re-export `yew-i18n`, `input_yew` && `yew-sidebar`

--- src/lib.rs
+++ src/lib.rs
@@ -7,3 +7,7 @@ pub use image::{Image, ImageProps};
 pub use yew::prelude;
 pub use yew::Renderer;
 pub use yew_router::prelude as router;
+pub use yew_navbar::{Menu, Navbar, NavbarProps};
+pub use yew_i18n::{I18nProvider, use_translation};
+pub use yew_sidebar::{MenuItem, Sidebar, SidebarProps};
+pub use input_yew::CustomInput as Input;
